9773626bcd1a37bfa47e9393676a9f2fa21deae1
YeongJin-Jo <yjjo97@naver.com>
2022-02-21 21:23:09 +0900

    유니티 입문 프로젝트 - ChickBall

---
+++ Chapter4 - ChickAttack/Assets/DestroyObj.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour
+{
+    public float deltaTime = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
